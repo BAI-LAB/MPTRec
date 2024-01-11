cbe1758d765564d87e46d2300804cf243f9b6112
huangle <3463763692@qq.com>
2024-01-11 11:15:47 +0800

    [OPT] CensusIncomeDataset添加新任务设置。

--- multitaskrec/dataset.py
+++ multitaskrec/dataset.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from torch.utils.data import Dataset
- 
 
-class AliCCPDataset(Dataset):  # Three tasks, namely CTR prediction, CVR prediction and BSI prediction
+
+class AliCCPDataset(
+    Dataset
+):  
     def __init__(self, datafile, data_size=-1):
+        '''Three tasks, namely CTR prediction, CVR prediction and BSI prediction
+        
+        Args:
+        
+        Returns:
+        '''
         super(AliCCPDataset, self).__init__()
         self.feature_names = []
         self.datafile = datafile
@@ -15,9 +23,9 @@ class AliCCPDataset(Dataset):  # Three tasks, namely CTR prediction, CVR predict
         print("start load data from: {}".format(self.datafile))
         count = 0
         with open(self.datafile) as f:
-            self.feature_names = f.readline().strip().split(',')[2:]
+            self.feature_names = f.readline().strip().split(",")[2:]
             for line in f:
-                line = line.strip().split(',')
+                line = line.strip().split(",")
                 line = [int(v) for v in line]
                 if line[-1] == 2:
                     line[-1] = 0
@@ -31,7 +39,9 @@ class AliCCPDataset(Dataset):  # Three tasks, namely CTR prediction, CVR predict
         self.feature_names = self.feature_names[1:-1]
         print("load data {} from {} finished".format(count, self.datafile))
 
-    def __len__(self, ):
+    def __len__(
+        self,
+    ):
         return len(self.data)
 
     def __getitem__(self, idx):
@@ -44,22 +54,64 @@ class AliCCPDataset(Dataset):  # Three tasks, namely CTR prediction, CVR predict
 
 
 class CensusIncomeDataset(Dataset):
-    def __init__(self, datafile):
-        self.feature_names = ['age', 'class_worker', 'det_ind_code', 'det_occ_code', 'education', 'wage_per_hour', 
-                              'hs_college', 'major_ind_code', 'major_occ_code', 'race', 'hisp_origin', 'sex', 
-                              'union_member', 'unemp_reason', 'full_or_part_emp', 'capital_gains', 'capital_losses', 
-                              'stock_dividends', 'tax_filer_stat', 'region_prev_res', 'state_prev_res', 'det_hh_fam_stat', 
-                              'det_hh_summ', 'instance_weight', 'mig_chg_msa', 'mig_chg_reg', 'mig_move_reg', 'mig_same',
-                              'mig_prev_sunbelt', 'num_emp', 'fam_under_18', 'country_father', 'country_mother', 
-                              'country_self', 'citizenship', 'own_or_self', 'vet_question', 'vet_benefits', 'weeks_worked', 
-                              'year']
+    def __init__(self, datafile, new_task):
+        self.feature_names = [
+            "age",
+            "class_worker",
+            "det_ind_code",
+            "det_occ_code",
+            "education",
+            "wage_per_hour",
+            "hs_college",
+            "major_ind_code",
+            "major_occ_code",
+            "race",
+            "hisp_origin",
+            "sex",
+            "union_member",
+            "unemp_reason",
+            "full_or_part_emp",
+            "capital_gains",
+            "capital_losses",
+            "stock_dividends",
+            "tax_filer_stat",
+            "region_prev_res",
+            "state_prev_res",
+            "det_hh_fam_stat",
+            "det_hh_summ",
+            "instance_weight",
+            "mig_chg_msa",
+            "mig_chg_reg",
+            "mig_move_reg",
+            "mig_same",
+            "mig_prev_sunbelt",
+            "num_emp",
+            "fam_under_18",
+            "country_father",
+            "country_mother",
+            "country_self",
+            "citizenship",
+            "own_or_self",
+            "vet_question",
+            "vet_benefits",
+            "weeks_worked",
+            "year",
+        ]
 
         df = pd.read_csv(
             datafile,
-            delimiter=',',
+            delimiter=",",
         )
-        self.feature_names.remove('education')
-        df['label_education'] = df.pop('education')
+        self.feature_names.remove(new_task)
+        if new_task == "education":
+            df[f"label_{new_task}"] = df[new_task].apply(lambda x: 1 if x == 9 else 0)
+        elif new_task == "sex":
+            df[f"label_{new_task}"] = df[new_task]
+        elif new_task == "race":
+            df[f"label_{new_task}"] = df[new_task].apply(lambda x: 1 if x == 0 else 0)
+        else:
+            raise ValueError(f"Invalid new task: {new_task}")
+        df.pop(new_task)
         self.data = df.values
 
     def __len__(self):
@@ -69,17 +121,30 @@ class CensusIncomeDataset(Dataset):
         line = self.data[idx]
         income = line[-3]
         marital = line[-2]
-        education = line[-1]
+        new_task = line[-1]
         features = dict(zip(self.feature_names, line[:-3]))
-        return income, marital, education, features
+        return income, marital, new_task, features
 
 
-class ByteRecDataset(Dataset):  # Three tasks: predicting finish, like, and duration time
+class ByteRecDataset(
+    Dataset
+):  # Three tasks: predicting finish, like, and duration time
     def __init__(self, datafile):
-        self.feature_names = ["uid", "user_city", "item_id", "author_id", "item_city", "channel", "music_id", "device"]
+        self.feature_names = [
+            "uid",
+            "user_city",
+            "item_id",
+            "author_id",
+            "item_city",
+            "channel",
+            "music_id",
+            "device",
+        ]
         self.datafile = datafile
-        df = pd.read_csv(self.datafile, delimiter=',', index_col=None)
-        df['duration_time'] = df['duration_time'].apply(lambda x: 1 if x > 0.000143 else 0)  # Videos longer than 10 seconds are labeled as 1
+        df = pd.read_csv(self.datafile, delimiter=",", index_col=None)
+        df["duration_time"] = df["duration_time"].apply(
+            lambda x: 1 if x > 0.000143 else 0
+        )  # Videos longer than 10 seconds are labeled as 1
         self.data = df.values
 
     def __len__(self):
